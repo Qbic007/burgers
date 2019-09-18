5956c4e62274afa7a0939ff1108a687d214923ac
Serg Pershin <pershin@exbico.ru>
2019-09-18 09:34:52 +0500

    yaMap added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
     accordeon('menu');
 
     function accordeon(blockName) {
-        let blockAccoTrigger = $('.' + blockName +'-acco__trigger');
+        let blockAccoTrigger = $('.' + blockName + '-acco__trigger');
         blockAccoTrigger.on('click', function (elem) {
             elem.preventDefault();
 
@@ -44,4 +44,60 @@ $(document).ready(function () {
             prevEl: '.slider__arrow-left',
         },
     });
+
+    // Карта
+
+    ymaps.ready(init);
+
+    var placemarks = [
+        {
+            latitude: 59.93,
+            longitude: 30.21,
+            hintContent: '<div class="map__hint">Ул. Красных фонарей д.152</div>',
+            balloonContent:
+                'Самые вкусные бургеры ЗДЕСЬ!',
+        },
+        {
+            latitude: 59.92,
+            longitude: 30.34,
+            hintContent: '<div class="map__hint">Ул. Сочных бургеров д.17</div>',
+            balloonContent:
+                'Самые вкусные бургеры У НАС!',
+        },
+
+        {
+            latitude: 59.87,
+            longitude: 30.27,
+            hintContent: '<div class="map__hint">Ул. Мясоедов д.19</div>',
+            balloonContent:
+                'Таких вкусных бургеров ВЫ ЕЩЕ НЕ ПРОБОВАЛИ!!',
+        },
+    ];
+
+    function init() {
+        var map = new ymaps.Map('map', {
+            center: [59.92, 30.31],
+            zoom: 11,
+            controls: ['zoomControl'],
+            behaviors: ['drag']
+        });
+
+        placemarks.forEach(function (obj) {
+            var placemark = new ymaps.Placemark([obj.latitude, obj.longitude], {
+                    hintContent: '<div class="map__hint">' + obj.hintContent + '</div>',
+                    balloonContent: obj.balloonContent,
+                },
+                {
+                    iconLayout: 'default#image',
+                    iconImageHref: './images/icons/map-marker.svg',
+                    iconImageSize: [44, 56],
+                    iconImageOffset: [-22, -56]
+
+                });
+
+            map.geoObjects.add(placemark);
+        });
+
+        map.setBounds(map.geoObjects.getBounds());
+    }
 });
